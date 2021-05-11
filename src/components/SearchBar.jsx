969528f4fc9fba4fcd0fcbd01ce7c374a692e52b
Bruna Campos <92brunacampos@gmail.com>
2021-05-11 13:18:30 -0300

    requisito 2, com issues no lint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const {
+    const { // Requisito 1
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
@@ -14,11 +14,8 @@ class SearchBar extends Component {
     } = this.props;
 
     return (
-      <div>
-
-      </div>
-
-    )
+      <form data-testid="search-bar-form" />
+    );
   }
 }
 
@@ -32,17 +29,3 @@ SearchBar.propTypes = {
 }.isRequired;
 
 export default SearchBar;
-
-// ### 1 - Crie um componente chamado `<SearchBar />`
-
-// Esse componente renderizará uma barra com filtros acima da listagem de cartões. Quais cartões serão mostrados no componente `<MovieList />` dependerá dos filtros escolhidos. `<SearchBar />` deve receber como props:
-
-//   - `searchText`, uma string
-//   - `onSearchTextChange`, uma callback
-//   - `bookmarkedOnly`, um boolean
-//   - `onBookmarkedChange`, uma callback
-//   - `selectedGenre`, uma string
-//   - `onSelectedGenreChange`, uma callback
-
-// **O que será verificado:**
-//   - Será validado se o componente `<SearchBar />`, recebendo as devidas props, renderiza com sucesso
