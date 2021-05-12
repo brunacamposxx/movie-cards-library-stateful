a84bf1c033e4064c4a9419755324afb54b4cebe4
Bruna Campos <92brunacampos@gmail.com>
2021-05-12 00:20:03 -0300

    Requisito 5, com muitos issues de eslint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,9 +34,22 @@ class SearchBar extends Component {
 					checked={ bookmarkedOnly }
 					onChange={ onBookmarkedChange }
 					data-testid="checkbox-input"
-
 					/>
 				</label>
+        {/*Requisito 5*/}
+        <label data-testid="select-input-label">
+          Filtrar por gênero
+          <select
+            data-testid="select-input"
+            value={ selectedGenre }
+            onChange={ onSelectedGenreChange }
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
