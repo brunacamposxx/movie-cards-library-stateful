5d1217c2b2018d04f57d945eaedd95ef5651c62a
Bruna Campos <92brunacampos@gmail.com>
2021-05-13 15:14:10 -0300

    criando componente AddMovie incompleto. Termino depois da aula ao vivo

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,19 @@
 // implement AddMovie component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends Component {
+  render() {
+    return(
+      <form>
+
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
+
+export default AddMovie;
