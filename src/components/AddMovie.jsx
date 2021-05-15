3fd28eafeaf7b90305e2fa517c66844bb152c9e4
Bruna Campos <92brunacampos@gmail.com>
2021-05-15 15:16:18 -0300

    requisito 6 e 7, criar e renderizar um form dentro do componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
-  render() {
-    return(
-      <form>
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form">
+        {}
       </form>
     );
   }
