b8cf7ed7d3aa59c29c8039bedd1a94c5a8604db3
Bruna Campos <92brunacampos@gmail.com>
2021-05-15 17:07:09 -0300

    Todos os inputs feitos. Lint reclamando de muitas linhas no render. Faltam as mudanças de estado

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,19 +2,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const initialState = {
+  subtitle: '',
+  title: '',
+  imagePath: '',
+  storyline: '',
+  rating: 0,
+  genre: 'action',
+};
+
 class AddMovie extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      subtitle: '',
-      title: '',
-      imagePath: '',
-      storyline: '',
-      rating: 0,
-      genre: 'action',
-    };
-
+    this.state = initialState;
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -39,6 +40,45 @@ class AddMovie extends Component {
             value={ subtitle }
             onChange={ this.handleChange }
           />
+        </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            data-testid="image-input"
+            value={ imagePath }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="storyline-input" data-testid="storyline-input-label">
+          Sinopse
+          <input
+            type="text"
+            data-testid="storyline-input"
+            value={ storyline }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="rating-input" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            data-testid="rating-input"
+            value={ rating }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            value={ genre }
+            onChange={ this.handleChange }
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
